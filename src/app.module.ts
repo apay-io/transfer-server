97cafd981ddbb355136ff7e0db0a3d305aa126e1
umbrel <s@apay.io>
2020-02-17 14:34:47 +1100

    Minimal admin interface
    Signed-off-by: umbrel <s@apay.io>

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import * as path from 'path';
 import { QueuesModule } from './queues/queues.module';
 import { UtilsModule } from './utils/utils.module';
 import { RedisModule } from 'nestjs-redis';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { RedisModule } from 'nestjs-redis';
     WalletsModule,
     QueuesModule,
     UtilsModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as rateLimit from 'express-rate-limit';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as exphbs from 'express-handlebars';
+import * as helpers from './utils/helpers';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   app.use(
@@ -15,6 +19,17 @@ async function bootstrap() {
       max: 100,
     }),
   );
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  const hbs = exphbs.create({
+    extname: 'hbs',
+    helpers,
+  });
+
+  app.engine('hbs', hbs.engine);
+  app.setViewEngine('hbs');
+
   await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
