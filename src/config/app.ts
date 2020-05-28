071def70a086cf2ddba248bd20e305a6dd3c094a
umbrel <s@apay.io>
2020-05-28 14:12:13 +1000

    Fix  tests
    Signed-off-by: umbrel <s@apay.io>

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,9 @@
-import {readFileSync} from 'fs';
+import { existsSync, readFileSync } from 'fs';
 
 export default {
   appName: 'apay.io',
-  companyInfo: readFileSync(process.cwd() + '/config/stellar.toml'),
+  companyInfo: existsSync(process.cwd() + '/config/stellar.toml')
+    ? readFileSync(process.cwd() + '/config/stellar.toml') : '',
   notificationSecrets: [
     process.env.NOTIFICATION_SECRET,
   ],
